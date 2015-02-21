08efd7b59be48a5313e8e3df13894d77e85a23d7
Kenji Vue <kenjivue@live.com>
2015-02-21 12:05:09 -0600

    updates 5/21

--- src/groovy/Users.groovy
+++ src/groovy/Users.groovy
@@ -1,7 +1,22 @@
 /**
  * Created by kenjivue on 2/20/15.
+ * This is the base user class
  */
 class Users {
-    def firstname
+    Long userid
+    String firstname
+    String lastname
+    String email
+    String password
+    Date datecreated
+    def constraints =
+            {
+                email(email:true)
+                password(blank:false, password:true)
+                firstname(blank:false)
+                lastname(blank:false)
+                datecreated(blank:false)
+
+            }
 
 }
